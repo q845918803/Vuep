9f13c436317e49cb280bfcec91a89d770e569b9e
Wap <war3hack@qq.com>
2019-07-03 18:36:13 +0800

    打卡

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,21 @@
 
-import Vue from 'vue';
+import Vue from 'vue'
 import App from '../view/App.vue'
-import VueRouter from 'vue-router'
 import router from './router'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import '../../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../../lib/mui/dist/css/mui.min.css'
 import '../css/index.less'
-import {Header} from 'mint-ui'
-import { Swipe, SwipeItem } from 'mint-ui';
+import {Header, Swipe, SwipeItem } from 'mint-ui'
 import '../../lib/mui/examples/hello-mui/css/icons-extra.css'
 
 
 Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
-Vue.use(VueRouter)
 Vue.component(Header.name,Header);
+Vue.use(VueRouter)
+Vue.use(VueResource)
 var app = new Vue({
     el:'#app',
     data(){
--- src/view/default.vue
+++ src/view/default.vue
@@ -1,10 +1,41 @@
 <template>
     <div>
-    <mt-swipe @change="handleChange">
-        <mt-swipe-item><h1>666666</h1></mt-swipe-item>
-        <mt-swipe-item><h1>222222</h1></mt-swipe-item>
-        <mt-swipe-item><h1>333333</h1></mt-swipe-item>
+    <mt-swipe :auto="4000">
+        <mt-swipe-item>
+            <img src="../img/banner01.jpg" alt="">
+        </mt-swipe-item>
+            <mt-swipe-item>
+            <img src="../img/banner02.jpg" alt="">
+        </mt-swipe-item>
+        <mt-swipe-item>
+            <img src="../img/banner03.jpg" alt="">
+        </mt-swipe-item>
+        <mt-swipe-item>
+            <img src="../img/banner04.jpg" alt="">
+        </mt-swipe-item>
     </mt-swipe>
+    <div class="mui-content">
+		        <ul class="mui-table-view mui-grid-view mui-grid-9">
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4"><a href="#">
+		                    <span class="mui-icon mui-icon-home"></span>
+		                    <div class="mui-media-body">Home</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4"><a href="#">
+		                    <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
+		                    <div class="mui-media-body">Email</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4"><a href="#">
+		                    <span class="mui-icon mui-icon-chatbubble"></span>
+		                    <div class="mui-media-body">Chat</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4"><a href="#">
+		                    <span class="mui-icon mui-icon-location"></span>
+		                    <div class="mui-media-body">location</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4"><a href="#">
+		                    <span class="mui-icon mui-icon-search"></span>
+		                    <div class="mui-media-body">Search</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4"><a href="#">
+		                    <span class="mui-icon mui-icon-phone"></span>
+		                    <div class="mui-media-body">Phone</div></a></li>
+		        </ul> 
+		</div>
     </div>
 </template>
 <script>
@@ -12,20 +43,26 @@
 export default {
     data(){
         return {
-
+            imglist:[
+                1,2,3,4
+            ]
         }
     },
+    created(){
+        
+    },
     methods:{
-        handleChange(index) {
-            return{
-                speed: 300,
-            }
-        }
+       
     },
 }
 </script>
-<style scoped>
-    .mint-swipe-items-wrap {
-        height: 300px;
+<style scoped text>
+    .mint-swipe{
+        height: 320px;
+    }
+    
+    .mint-swipe-items-wrap img {
+        height: 100%;
+        width: 100%;
     }
 </style>
